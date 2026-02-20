5f9fe7885606c35e118ae16237c6493d8169f792
afnan328 <muhammedafnan4791@gmail.com>
2026-02-20 15:12:12 +0530

    first commit

---
+++ components/sections/home1/Brand.js
@@ -0,0 +1,151 @@
+import BrandSlider1 from '@/components/slider/BrandSlider1'
+import CounterUp from "@/components/elements/CounterUp"
+export default function Brand() {
+    return (
+        <>
+  {/*Brand One Start*/}
+  <section className="brand-one" style={{marginBottom:'140px',marginTop:'-30px'}}>
+    <div className="container">
+      {/* <h2 className="brand-one__text count-box">
+      Our Clients {" "}
+        <CounterUp end={40} />
+        <span>+</span> 
+      </h2> */}
+      <h2 class="section-title__title" style={{textAlign:'center',margin:'20px'}}>Our Clients</h2>
+      <BrandSlider1/>
+    </div>
+
+    <div className="project-details__img-box" style={{marginTop:'30px',margin:'30px',marginBottom:'-30px'}}>
+                <div className="row">
+                <h2 class="section-title__title" style={{textAlign:'center',margin:'20px'}}>Our  Projects  </h2>
+                    {/* <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/project/KAFDnew.webp"
+                        alt=""
+                        />
+                        <h3 style={{textAlign:'center',margin:'10px'}}>KAFD Project</h3>
+                    </div>
+                    </div>
+                    <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/project/Kingnew.webp"
+                        alt=""
+                        />
+                        <h3 style={{textAlign:'center',margin:'10px'}}>King Salman Park Project
+                        </h3>
+                    </div>
+                    </div>
+                    <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/project/Qiddiyanew.webp"
+                        alt=""
+                        />
+                        <h3 style={{textAlign:'center',margin:'10px'}}> Qiddiya Project
+                        </h3>
+                    </div>
+                    </div> */}
+
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/1.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/2.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/3.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/4.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/5.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/6.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/7.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/8.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                     <div className="col-xl-4">
+                    <div className="project-details__img-box-img">
+                        <img
+                        src="./assets/images/allimgs/new/9.webp"
+                        alt=""
+                        />
+                        
+                    </div>
+                    </div>
+
+                    
+                   
+                </div>
+</div>
+  </section>
+  {/*Brand One End*/}
+</>
+
+    )
+}
